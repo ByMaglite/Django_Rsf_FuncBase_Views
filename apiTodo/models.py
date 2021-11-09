21485611c98c5fb779feb421e1aa4ba4ba71c403
ByMaglite <fceyhan55@gmail.com>
2021-11-09 13:05:45 +0300

    models created

--- apiTodo/models.py
+++ apiTodo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    task = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    
+    TITLE = (
+        ('H', 'High'),
+        ('M', 'Medium'),
+        ('L', 'Low')
+    )
+    priority = models.CharField(max_length=50, choices=TITLE, default="L")
+    
+    done = models.BooleanField(default=False)
